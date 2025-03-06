a2f95c4e1744e2b2e4c93607d5da92d319fd0627
Vikas Bargal <164344040+3105Vikcky@users.noreply.github.com>
2025-03-06 15:39:42 +0530

    Update cal.py
    Added the Modulus in calculator so we can easily calculate Modulous

--- cal.py
+++ cal.py
@@ -9,14 +9,17 @@ def multiply(x, y):
 
 def divide(x, y):
     return x / y if y != 0 else "Error! Division by zero."
+def modulus(x.y):
+    return x%2
+
 
 print("Select operation:")
 print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
-
-choice = input("Enter choice (1/2/3/4): ")
+print("5. Moduolus")
+choice = input("Enter choice (1/2/3/4/5): ")
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
@@ -29,5 +32,7 @@ elif choice == '3':
     print(f"Result: {multiply(num1, num2)}")
 elif choice == '4':
     print(f"Result: {divide(num1, num2)}")
+elif choice=='5':
+    print(f"Result: {modulus(num1,num2)}")
 else:
     print("Invalid input")
